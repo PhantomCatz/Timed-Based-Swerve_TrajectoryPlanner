1e19b33a72d7f0d4c8c305f25bd142e6f24068a5
infinitystraycat <60206236+infinitystraycat@users.noreply.github.com>
2023-08-12 12:03:50 -0700

    8/12

---
+++ TrajectoryPlanner - Copy - Copy/src/main/java/frc/robot/CatzConstants.java
@@ -0,0 +1,49 @@
+package frc.robot;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
+public class CatzConstants {
+    private static final double MODULE_DISTANCE_FROM_CENTER = 0.42672;
+
+    private static final Translation2d SWERVE_LEFT_FRONT_LOCATION  = new Translation2d(MODULE_DISTANCE_FROM_CENTER, MODULE_DISTANCE_FROM_CENTER).div(Math.sqrt(2));
+    private static final Translation2d SWERVE_LEFT_BACK_LOCATION   = new Translation2d(MODULE_DISTANCE_FROM_CENTER, -MODULE_DISTANCE_FROM_CENTER).div(Math.sqrt(2));
+    private static final Translation2d SWERVE_RIGHT_FRONT_LOCATION = new Translation2d(-MODULE_DISTANCE_FROM_CENTER, MODULE_DISTANCE_FROM_CENTER).div(Math.sqrt(2));
+    private static final Translation2d SWERVE_RIGHT_BACK_LOCATION  = new Translation2d(-MODULE_DISTANCE_FROM_CENTER, -MODULE_DISTANCE_FROM_CENTER).div(Math.sqrt(2));
+
+    // calculates the orientation and speed of individual swerve modules when given the motion of the whole robot
+    public static final SwerveDriveKinematics swerveDriveKinematics = new SwerveDriveKinematics(
+        SWERVE_LEFT_FRONT_LOCATION,
+        SWERVE_LEFT_BACK_LOCATION,
+        SWERVE_RIGHT_FRONT_LOCATION,
+        SWERVE_RIGHT_BACK_LOCATION
+    );
+
+    public static final double MAX_SPEED = 4.0;
+
+    public static final double SDS_L1_GEAR_RATIO = 8.14;       //SDS mk4i L1 ratio reduction
+    public static final double SDS_L2_GEAR_RATIO = 6.75;       //SDS mk4i L2 ratio reduction
+    
+    public static final double DRVTRAIN_WHEEL_DIAMETER             = 0.095;
+    public static final double DRVTRAIN_WHEEL_CIRCUMFERENCE        = (Math.PI * DRVTRAIN_WHEEL_DIAMETER);
+
+    //uses a trapezoidal velocity/time graph enforced with a PID loop
+    private static ProfiledPIDController autoTurnPIDController
+            = new ProfiledPIDController(2, 0, 0, new TrapezoidProfile.Constraints(8, 8));
+
+    static{
+        autoTurnPIDController.enableContinuousInput(-Math.PI, Math.PI); //offset clamped between these two values
+        autoTurnPIDController.setTolerance(Math.toRadians(10)); //tolerable error
+    }
+
+    // calculates target chassis motion when given current position and desired trajectory
+    public static final HolonomicDriveController holonomicDriveController = new HolonomicDriveController(
+        new PIDController(0.15, 0, 0), // PID values for x offset
+        new PIDController(0.15, 0, 0), // PID values for y offset
+        autoTurnPIDController // PID values for orientation offset
+    );
+}
---
+++ TrajectoryPlanner - Copy - Copy/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,88 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.Autonomous.AutonActionExecutor;
+import frc.Autonomous.AutonRoutineSelector;
+import frc.Autonomous.CatzRobotTracker;
+import frc.Mechanisms.CatzDrivetrain;
+
+@SuppressWarnings("unused")
+public class Robot extends TimedRobot {
+  private final SendableChooser<String> autoChooser = new SendableChooser<>();
+  private final SendableChooser<String> sideChooser = new SendableChooser<>();
+
+  public static final CatzDrivetrain drivetrain = CatzDrivetrain.getInstance();
+  private final CatzRobotTracker robotTracker = CatzRobotTracker.getInstance();
+
+  public static final AutonActionExecutor autonExecutor = AutonActionExecutor.getInstance();
+  private final AutonRoutineSelector autonRoutineSelector = AutonRoutineSelector.getInstance();
+
+  private static final Timer autonTimer = new Timer();
+  @Override
+  public void robotInit()
+  {
+    // AutonomousContainer.getInstance().initialize(
+    //             true,
+    //             new CommandTranslator(
+    //                     auton::setAutoPath,
+    //                     auton::stopMovement,
+    //                     auton::setAutoRotation,
+    //                     auton::isFinished,
+    //                     auton::getAutoElapsedTime,
+    //                     robotTracker::resetPosition,
+    //                     true
+    //             ),
+    //             false,
+    //             this
+    //     );
+
+    //   AutonomousContainer.getInstance().getAutonomousNames().forEach(name -> autoChooser.addOption(name, name));
+
+    //   sideChooser.setDefaultOption("Blue", "blue");
+    //   sideChooser.addOption("Red", "red");
+
+    //   SmartDashboard.putData("Path Choices", autoChooser);
+    //   SmartDashboard.putData("Red or Blue", sideChooser);
+    robotTracker.resetPosition(new Pose2d());
+  }
+
+  @Override
+  public void autonomousInit()
+  {
+    // autonTimer.reset();
+    // autonTimer.start();
+
+    // AutonomousContainer.getInstance().runAutonomous(autoChooser.getSelected(), sideChooser.getSelected(), true);
+
+    robotTracker.resetPosition(new Pose2d());
+    autonRoutineSelector.updateSelectedRoutine();
+    autonExecutor.start();    
+  }
+
+  @Override
+  public void disabledInit()
+  {
+    autonTimer.stop();
+    autonExecutor.stop();
+    AutonActionExecutor.resetInstance();
+  }
+
+  @Override
+  public  void testInit()
+  {
+    drivetrain.initializeOffsets();
+  }
+
+  public static Timer getAutonTimer()
+  {
+    return autonTimer;
+  }
+}
